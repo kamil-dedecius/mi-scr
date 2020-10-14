89d146c4c5c64b0200d49f30f440dc228d082739
Kamil Dedecius <kamil.dedecius@fit.cvut.cz>
2020-10-14 12:12:02 +0200

    unit root testy

---
+++ zdrojaky/ur_tests.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from statsmodels.tsa.stattools import adfuller, kpss
+
+def adf_test(timeseries):
+    print ('Results of Dickey-Fuller Test:')
+    print('H0: Unit root (nonstationarity) present')
+    print('HA: No unit root')
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+       dfoutput['Critical Value (%s)'%key] = value
+    print (dfoutput)
+
+
+def kpss_test(timeseries):
+    print ('Results of KPSS Test:')
+    print('H0: Trend stationary')
+    print('HA: Not trend stationary (has unit root)')
+    kpsstest = kpss(timeseries, regression='c', nlags="auto")
+    kpss_output = pd.Series(kpsstest[0:3], index=['Test Statistic','p-value','Lags Used'])
+    for key,value in kpsstest[3].items():
+        kpss_output['Critical Value (%s)'%key] = value
+    print (kpss_output)
+
