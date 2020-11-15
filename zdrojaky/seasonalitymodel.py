49087353103aa836cb095cc0c2f1d19db5557b3c
Kamil Dedecius <kamil.dedecius@fit.cvut.cz>
2020-11-15 17:16:15 +0100

    zabudka...

---
+++ zdrojaky/seasonalitymodel.py
@@ -0,0 +1,1 @@
+sm-seasonality-model.py
--- zdrojaky/ur_tests.py
+++ zdrojaky/ur_tests.py
@@ -10,15 +10,16 @@ def adf_test(timeseries):
     for key,value in dftest[4].items():
        dfoutput['Critical Value (%s)'%key] = value
     print (dfoutput)
+    print()
 
 
 def kpss_test(timeseries):
     print ('Results of KPSS Test:')
     print('H0: Trend stationary')
     print('HA: Not trend stationary (has unit root)')
-    kpsstest = kpss(timeseries, regression='c', nlags="auto")
+    kpsstest = kpss(timeseries, regression='ct', nlags="auto")
     kpss_output = pd.Series(kpsstest[0:3], index=['Test Statistic','p-value','Lags Used'])
     for key,value in kpsstest[3].items():
         kpss_output['Critical Value (%s)'%key] = value
     print (kpss_output)
-
+    print()
